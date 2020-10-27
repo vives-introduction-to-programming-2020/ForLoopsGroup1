3024e139b3d4a2e58bac79d2f4228f71cd82ed0e
Nico De Witte <nico.dewitte@vives.be>
2020-10-27 07:22:22 +0100

    Code of classroom

---
+++ ForLoops/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello " + i);
+            }
+
+            // Declaring k outside of for-loop
+            int k;
+            for (k = 0; k < 10; k++)
+            {
+                Console.WriteLine("Hello " + k);
+            }
+            Console.WriteLine("Hello " + k);
+
+            // Counting +2 instead of +1
+            for (int j = 0; j < 10; j=j+2)
+            {
+                Console.WriteLine("Hello " + j);
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Class Demo - For Loops - Group 1
+
+Demo code of for-loops.
+
+## How to get this locally:
+
+Go to a local directory on your system and open Powershell in that directory. Now clone this repo using git:
+
+```bash
+git clone git@github.com:vives-introduction-to-programming-2020/CircleAppGroup2.git
+```
